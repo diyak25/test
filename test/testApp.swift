0c4c512be1bc4414a38671d6cd1aa7471343e86f
Diya K <diya.kumar25@gmail.com>
2022-08-04 08:17:05 -0400

    Initial Commit

---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by Diya Kumar on 8/4/22.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
